5ce1e4df892d9cb72892f3a4938605021e0ccbce
peggy <1083341411@qq.com>
2022-02-04 21:55:05 +0800

    Site updated: 2022-02-04 21:55:05

--- js/dark.js
+++ js/dark.js
@@ -1,26 +1,12 @@
-(function () {
+(function() {
     /**
      * Icarus 夜间模式 by iMaeGoo
      * https://www.imaegoo.com/
-     */
-    function isNightFun() {
-        var isNightTemp = localStorage.getExpire('dark');
+     */ 
 
-        // 第一次进来判断是白天还是晚上
-        if (isNightTemp == null || isNightTemp == undefined) {
-            if (isNightRange("19:00", "23:59") || isNightRange("00:00", "07:00")) {
-                isNightTemp = 'true';
-            } else {
-                isNightTemp = 'false';
-            }
-            localStorage.setExpire("dark", isNightTemp, expireTime1H);
-        }
-        return isNightTemp;
-    }
-
-    var isNight = isNightFun();
+    var isNight = 'true';
     var darkNav;
-
+  
     function applyNight(value) {
         if (value.toString() === 'true') {
             document.body.classList.remove('light');
@@ -30,7 +16,7 @@
             document.body.classList.add('light');
         }
     }
-
+  
     function findNightNav() {
         darkNav = document.getElementById('dark-nav');
         if (!darkNav) {
@@ -39,13 +25,13 @@
             darkNav.addEventListener('click', switchNight);
         }
     }
-
+  
     function switchNight() {
         isNight = isNight ? isNight.toString() !== 'true' : true;
         applyNight(isNight);
         localStorage.setItem('dark', isNight);
     }
-
+  
     findNightNav();
     isNight && applyNight(isNight);
-}());
+  }());
